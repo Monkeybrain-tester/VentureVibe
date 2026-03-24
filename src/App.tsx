6601dd0aadd7fd6d43b66e603e1f5311d1048542
matthewlosito <mlosito@ufl.edu>
2026-03-24 00:45:04 -0400

    Added Authentication Page and Logout Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import MapPage from './pages/MapPage';
 import AuthPage from './pages/AuthPage';
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext'
 
 function App() {
   const { user } = useAuth()
 
-  if (!user) {
-    return <AuthPage />
-  }
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MapPage />}/>
+        <Route path="/" element={user ? <MapPage /> : <AuthPage />}/>
+        <Route path="*" element={<Navigate to="/" replace />}/>
       </Routes>
     </BrowserRouter>
   )
