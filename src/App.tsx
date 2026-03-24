deb78d282cf3ce51a427ba82f8b71ed872399c97
matthewlosito <mlosito@ufl.edu>
2026-03-24 00:03:02 -0400

    Added Authentication Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import MapPage from './pages/MapPage';
+import AuthPage from './pages/AuthPage';
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useAuth } from './AuthContext'
 
 function App() {
+  const { user } = useAuth()
+
+  if (!user) {
+    return <AuthPage />
+  }
 
   return (
     <BrowserRouter>
